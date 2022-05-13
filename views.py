c051bd448f317efe0ed3f2e7a6fb06d7b1bd1592
Shamankub <shaman_kub@mail.ru>
2022-05-14 00:54:38 +0300

    Lesson 1 done.

---
+++ views.py
@@ -0,0 +1,21 @@
+from framework.templator import render
+
+
+class Index:
+    def __call__(self, request):
+        return "200 OK", render("index.html", date=request.get("date", None))
+
+
+class About:
+    def __call__(self, request):
+        return "200 OK", render("about.html", date=request.get("date", None))
+
+
+class Price:
+    def __call__(self, request):
+        return "200 OK", render("price.html", date=request.get("date", None))
+
+
+class Contacts:
+    def __call__(self, request):
+        return "200 OK", render("contacts.html", date=request.get("date", None))
