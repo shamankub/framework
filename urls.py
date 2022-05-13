c8e0d555ca68b78cbcedc60376f48532b4827b2c
Антон Есенеев <84543711+shamankub@users.noreply.github.com>
2022-05-14 00:57:17 +0300

    Lesson 1 done. (#1)

---
+++ urls.py
@@ -0,0 +1,22 @@
+from datetime import date
+
+from views import About, Contacts, Index, Price
+
+
+# front controller
+def secret_front(request):
+    request["date"] = date.today()
+
+
+def other_front(request):
+    request["key"] = "key"
+
+
+fronts = [secret_front, other_front]
+
+routes = {
+    "/": Index(),
+    "/about/": About(),
+    "/price/": Price(),
+    "/contacts/": Contacts(),
+}
